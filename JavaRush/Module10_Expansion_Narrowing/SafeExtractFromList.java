e7c3fd48c73fb8171a35c0f273350c369bb95525
Archie <avatar10.01@bk.ru>
2021-12-08 18:30:54 +0300

    fin_module10

--- JavaRush/Module10_Expansion_Narrowing/SafeExtractFromList.java
+++ JavaRush/Module10_Expansion_Narrowing/SafeExtractFromList.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.SplittableRandom;
 
 /**
  * Создать список целых чисел.
@@ -20,51 +17,48 @@ public class SafeExtractFromList {
 
     static class MyException extends java.lang.RuntimeException {
         String str = new String("ERROR: Invalid NumberFormat !");
+        int val = 0;
+
+        public MyException ( int defVal ) {
+            this.val = defVal;
+        }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         ArrayList<Integer> arrayList;
         int                err = 0;
 
+        arrayList = writeToArray();
         try {
-            arrayList = writeToArray();
-            err = safeGetElement(arrayList, 2, -500);
+            err = safeGetElement(arrayList, 12, -500);
         } catch (MyException e) {
-            System.out.println(e.str);
-        } catch (Throwable e)
-        {
-            System.out.println("!!!");
+            System.out.println("ERROR: " + e.val);
         }
+
         System.out.println(err);
 
     }
 
     public static int safeGetElement(ArrayList<Integer> list, int index, int defaultValue) throws MyException {
-        try {
-            for (int i = 0; i < list.size(); ++i) {
-                if (i == index) {
-                    return list.get(index);
-                }
-            }
-            return ((Integer)(Object)new String("HUi"));
-        }catch (Throwable e){
-            return defaultValue;
+        if (index >= list.size() || index < 0) {
+            throw new MyException(defaultValue);
         }
+
+        for (int i = 0; i < list.size(); ++i) {
+            if (i == index) {
+                return list.get(index);
+            }
+        }
+        return defaultValue;
     }
 
     public static ArrayList<Integer> writeToArray () throws IOException, MyException {
         BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
-        try {
             for (int i = 0; i < size; ++i) {
                 System.out.print("Enter number (" + (i + 1) + "/" + size + "): ");
                 arrayList.add(Integer.parseInt(buff.readLine()));
             }
-        }catch (Throwable e)
-        {
-            throw new MyException();
-        }
-
         return arrayList;
     }
 }
