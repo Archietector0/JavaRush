df0daee3c4cd82198d98632061f39e1eb8e7f7d8
Archie <avatar10.01@bk.ru>
2021-12-11 22:07:34 +0300

    Point_14

---
+++ JavaRush/Module14_TypeCasting_Instanceof/Currencies/Hrivna.java
@@ -0,0 +1,13 @@
+package Currencies;
+
+public class Hrivna extends Money {
+
+    public Hrivna(double amount) {
+        super(amount);
+    }
+
+    public String getCurrencyName () {
+        return "HRN";
+    }
+
+}
---
+++ JavaRush/Module14_TypeCasting_Instanceof/Currencies/USD.java
@@ -0,0 +1,12 @@
+package Currencies;
+
+public class USD extends Money {
+
+    public USD (double amount) {
+        super(amount);
+    }
+
+    public String getCurrencyName() {
+        return "USD";
+    }
+}
